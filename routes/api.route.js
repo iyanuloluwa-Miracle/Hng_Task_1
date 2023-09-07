92f29557a894962e131eaef694fe51d3db569897
iyanuloluwa-Miracle <iyanudina@gmail.com>
2023-09-07 01:44:56 -0700

    added some changes

--- routes/api.route.js
+++ routes/api.route.js
@@ -1,9 +1,9 @@
 const router = require('express').Router();
 const apiController = require('../controller/apiController');
 
-router.get('/', async (req, res, next) => {
-  res.send({ message: 'Ok api is working 🚀' });
-});
-router.get('/hng', apiController.getApiData);
+// router.get('/', async (req, res, next) => {
+//   res.send({ message: 'Ok api is working 🚀' });
+// });
+router.get('', apiController.getApiData);
 
 module.exports = router;
